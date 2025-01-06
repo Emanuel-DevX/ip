cb9eb977c758888f822f0ebecb519739b07dad9f
Emanuel-DevX <jefymoxi@gmail.com>
2025-01-06 02:45:20 -0800

    Added a couple more exercises

---
+++ script.js
@@ -0,0 +1,12 @@
+function hello() {
+    alert("Hello how are you doing?");
+  }
+  
+  function showtime() {
+    var now = new Date();
+    alert("It is " + now);
+  }
+  function openIBM(){
+    window.open("https://www.ibm.com");
+    
+  }
